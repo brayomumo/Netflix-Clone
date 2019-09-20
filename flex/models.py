fbd75da64e0d0dc016821ec145a7884b646db667
Brian <brayomumo5@gmail.com>
2019-09-20 17:57:47 +0300

    Create main class

---
+++ flex/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Create your models here.
+class Movie:
+    '''
+    Movie class to define Movie Objects
+    '''
+
+    def __init__(self,id,title,overview,poster,vote_average,vote_count,age,percent):
+        self.id =id
+        self.title = title
+        self.overview = overview
+        self.poster = 'https://image.tmdb.org/t/p/w500/'+ poster
+        self.vote_average = vote_average
+        self.vote_count = vote_count
+        self.age = age
+        self.percent = percent
